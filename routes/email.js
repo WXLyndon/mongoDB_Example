2524864f4e3691f94ef749e7d6e527e890637d57
Xuanlin Wang <xuanlin.wang@mail.utoronto.ca>
2019-06-12 14:53:07 -0400

    Add get method to check if the email exists in db.

--- routes/email.js
+++ routes/email.js
@@ -2,14 +2,14 @@ let express = require('express');
 let router = express.Router();
 let EmailModel = require('../models/email');
 let mongoose = require('mongoose');
-let shortid = require('shortid');
+let shortId = require('shortid');
 
 /* GET users listing. */
-router.post('/post', function(req, res, next) {
+router.post('/', function(req, res) {
   let msg = new EmailModel({
     _id: new mongoose.Types.ObjectId,
-    id: shortid.generate(),
-    email: req.body.email,
+    id: shortId.generate(),
+    email: req.body.email
   });
   msg.save()
       .then(result => {
@@ -20,6 +20,21 @@ router.post('/post', function(req, res, next) {
         console.log(err);
         res.status(500).json({error: err});
       });
+});
+
+router.get('/', function (req, res) {
+    EmailModel
+        .find({
+            email: req.body.email
+        })
+        .then( doc => {
+            console.log(doc);
+            res.status(201).json({doc});
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({error: err});
+        });
 });
 
 module.exports = router;
