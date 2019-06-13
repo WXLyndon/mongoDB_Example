d6fea096ec3bb26a84449bbaaf90d54de612a8dd
Xuanlin Wang <xuanlin.wang@mail.utoronto.ca>
2019-06-13 19:07:58 -0400

    Add post method to save the user on db.

--- models/user.js
+++ models/user.js
@@ -1,10 +1,12 @@
 let mongoose = require('mongoose');
 
 let userSchema = new mongoose.Schema({
+    _id: mongoose.Types.ObjectId,
+    id: {type: String, unique: true},
     firstName: String,
     lastName: String
 });
-userSchema.virtual('fullName').get(function(){
+userSchema.virtual('fullName').get(function () {
     return this.firstName + ' ' + this.lastName;
 });
 
@@ -13,5 +15,9 @@ userSchema.virtual('fullName').set(function (name) {
     this.firstName = str[0];
     this.lastName = str[1];
 });
+
+userSchema.methods.getInitials = function () {
+  return this.firstName[0] + this.lastName[0];
+};
 
 module.exports = mongoose.model('User', userSchema);
--- routes/user.js
+++ routes/user.js
@@ -1,14 +1,36 @@
 let express = require('express');
 let router = express.Router();
 let UserModel = require('../models/user.js');
+let shortId = require('shortid');
+let mongoose = require('mongoose');
+
+/* Post user record to the db */
+router.post('/', function (req, res) {
+    let user = new UserModel({
+        _id: new mongoose.Types.ObjectId,
+        id: shortId.generate(),
+        lastName: req.body.lastName,
+        firstName: req.body.firstName
+    });
+    user.save()
+        .then(result => {
+            console.log(result);
+            res.status(201).json({result: 'success'});
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({error: err});
+        });
+});
 
 router.get('/', function (req, res) {
-    let user = new UserModel();
+    let user = new UserModel({
+    });
     user.fullName = 'Lyndon Wang';
     console.log(user.toJSON());
     console.log();
-    console.log(user.fullName);
-    res.json(user.toJSON());
+    console.log(user.getInitials());
+    res.json(user.getInitials());
 });
 
 module.exports = router;
